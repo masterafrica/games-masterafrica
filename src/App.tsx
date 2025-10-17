10a44cc8c878156117a78a79edb9385500d9fefd
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-10-17 15:47:48 +0100

    chore: setup auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
+import AuthLayout from "./layouts/auth-layout";
+import LoginPage from "./pages/auth/login";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route element={<LoginPage />} path="login" />
+      </Route>
     </Routes>
   );
 }
