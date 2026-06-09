1cd8457d93413ca2aae3fdbff81f932d5ce56ed1
prince <princewillasotibe123@gmail.com>
2026-06-09 13:25:35 +0100

    used vps

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,3 +1,4 @@
 export const isProduction =true
 
-export const BASE_API_URL = isProduction?"https://master-api-mun0.onrender.com":"http://localhost:8000";
+export const BASE_API_URL = isProduction?"http://72.62.1.91:8000":"http://localhost:8000";
+// export const BASE_API_URL = isProduction?"https://master-api-mun0.onrender.com":"http://localhost:8000";
